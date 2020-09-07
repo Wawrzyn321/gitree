607743cc68506321bff1bd347d65a4b07e14f176
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-09-07 20:36:12 +0200

    Updates

---
+++ precache-manifest.26a1f3585a64416d494e617abd64907b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1aeb41cc3650a88325db21e9c540c3",
+    "url": "/gitree/index.html"
+  },
+  {
+    "revision": "c9ffc27b54b2d89bd154",
+    "url": "/gitree/static/css/2.bab5d168.chunk.css"
+  },
+  {
+    "revision": "48ae31bc959c83c0f6ef",
+    "url": "/gitree/static/css/main.1c3af458.chunk.css"
+  },
+  {
+    "revision": "c9ffc27b54b2d89bd154",
+    "url": "/gitree/static/js/2.50e863b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitree/static/js/2.50e863b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ae31bc959c83c0f6ef",
+    "url": "/gitree/static/js/main.5b71c6ca.chunk.js"
+  },
+  {
+    "revision": "0a0a3f0531dda5b23128",
+    "url": "/gitree/static/js/runtime-main.575ba454.js"
+  }
+]);
