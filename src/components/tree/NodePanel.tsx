4d1babb0ab0b5aaa1048f9022793b30fdc73db7a
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2025-04-28 08:45:29 +0200

    Update pages.yml

--- src/components/tree/NodePanel.tsx
+++ src/components/tree/NodePanel.tsx
@@ -26,6 +26,7 @@ export function NodePanel({ node, showLink }: NodePanelProps) {
       <h5>
         <FontAwesomeIcon color={actionColor} icon={icon} />
         Name: {node.path}
+        {/* @ts-ignore */}
         {showLink && <NodeLink node={node} />}
       </h5>
       <p>Size: {filesize(node.size)}</p>
